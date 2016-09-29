87be0704dba69455d3ecf7d7e08b1d81a5781224
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2016-09-29 14:33:24 +0200

    add mac support

--- digit_test.py
+++ digit_test.py
@@ -1,31 +1,64 @@
 #! /usr/bin/python2
-
 from __future__ import print_function
 from PIL import Image, ImageDraw, ImageFont, ImageFilter, ImageTransform
 import random
 import glob, os, os.path
 import numpy as np
+from sys import platform
 
-font_blacklist = ("FiraMono", "Corben", "D050000L","jsMath", "Redacted",
-  "RedactedScript", "AdobeBlank", "EncodeSans", "cwTeX", "Droid", "Yinmar",
-  "Lao")
+# Get list of font locations. Additional fonts can be added to "fonts" in the
+# working directory.
+def font_dirs():
+    if platform == "linux2":
+        return ("/usr/share/fonts/*", "./fonts")
+    elif platform == "darwin":
+        return ("/Library/Fonts", "/System/Library/Fonts", os.path.expanduser("~/Library/Fonts"), "./fonts")
+    else:
+        raise RuntimeError('unsupported OS')
 
+# Blacklist symbol fonts and fonts not working with PIL
 def is_font_blacklisted(font_file):
+    font_blacklist = (
+        # Linux fonts
+        "FiraMono", "Corben", "D050000L","jsMath", "Redacted",
+        "RedactedScript", "AdobeBlank", "EncodeSans", "cwTeX", "Droid", "Yinmar", "Lao",
+        # Apple fonts
+        "Apple Braille", "NISC18030", "Wingdings", "Webdings", "LastResort",
+        "Bodoni Ornaments", "Hoefler Text Ornaments", "ZapfDingbats", "Kokonor")
+
     font_family = os.path.basename(font_file).split(".")[0].split("-")[0]
     return font_family.startswith(font_blacklist)
 
-#load all ttf fonts
-font_array = []
-for font_file in glob.iglob("/usr/share/fonts/*/*.ttf"):
-    if not is_font_blacklisted(font_file):
-        font_array.append((font_file, ImageFont.truetype(font=font_file, size=16)))
-    else:
-        print("Skipping blacklisted font: %s" % font_file)
+# Collect all ttf fonts in one font location, except those blacklisted.
+def find_fonts_in_directory(directory_path):
+    font_array = []
+    for font_file in glob.iglob(directory_path + "/*.ttf"):
+        if not is_font_blacklisted(font_file):
+            try:
+                font_array.append((font_file, ImageFont.truetype(font=font_file, size=16)))
+                print("adding font: %s" % font_file)
+            except IOError:
+                print("Error loading font: %s" % font_file)
+        else:
+            print("Skipping blacklisted font: %s" % font_file)
+
+    return font_array
+
+def find_fonts():
+    font_array = []
+    for font_dir in font_dirs():
+        font_array += find_fonts_in_directory(font_dir)
+
+    return font_array
+
+font_array = find_fonts()
 
 digit_size = 32
 num_digit_columns = 32
 num_digit_rows = 32
 debug = False
+num_fonts = len(font_array)
+current_font = 0
 
 overview_image = Image.new("L", (digit_size * num_digit_columns, digit_size * num_digit_rows), 255)
 overview_draw = ImageDraw.Draw(overview_image)
@@ -48,12 +81,17 @@ def displacement():
 def create_digit(font_tuple, digit):
     font = font_tuple[1]
     font_name = font_tuple[0]
-    background_color = random.randint(0,255)
-    while True:
-        text_color = random.randint(0,255)
-        # find a text color that has a minimum amount of contrast against background_color:
-        if abs(text_color - background_color) > 16:
-            break
+
+    if debug:
+        background_color = 255
+        text_color = 0
+    else:
+        background_color = random.randint(0,255)
+        while True:
+            text_color = random.randint(0,255)
+            # find a text color that has a minimum amount of contrast against background_color:
+            if abs(text_color - background_color) > 16:
+                break
 
     digit_image = Image.new("L", (2 * digit_size, 2 * digit_size), background_color)
     draw = ImageDraw.Draw(digit_image)
@@ -70,16 +108,16 @@ def create_digit(font_tuple, digit):
 
     draw.text((x,y), str(digit), font=font, fill=text_color)
 
-    bounding_box = np.array([0,0,0,1,1,1,1,0]) * digit_size * 2 + (2 * np.random.rand(8) - 1) * digit_size / 4
-    transformation = ImageTransform.QuadTransform(bounding_box)
-    digit_image = digit_image.transform((digit_size * 2, digit_size * 2), transformation, resample=Image.BICUBIC)
+    if not debug:
+        bounding_box = np.array([0,0,0,1,1,1,1,0]) * digit_size * 2 + (2 * np.random.rand(8) - 1) * digit_size / 4
+        transformation = ImageTransform.QuadTransform(bounding_box)
+        digit_image = digit_image.transform((digit_size * 2, digit_size * 2), transformation, resample=Image.BICUBIC)
 
-    angle = random.randrange(-15,15)
-    digit_image = digit_image.rotate(angle, resample=Image.BICUBIC, expand = 0)
+        angle = random.randrange(-15,15)
+        digit_image = digit_image.rotate(angle, resample=Image.BICUBIC, expand = 0)
+        digit_image = digit_image.filter(ImageFilter.GaussianBlur(radius=1.5 * random.random()))
 
     digit_image = digit_image.crop((digit_size/2, digit_size/2, digit_size * 3 / 2, digit_size * 3 / 2))
-
-    digit_image = digit_image.filter(ImageFilter.GaussianBlur(radius=1.5 * random.random()))
 
     if debug:
         draw = ImageDraw.Draw(digit_image)
@@ -91,7 +129,16 @@ default_font = ImageFont.load_default()
 
 for i in range(0,num_digit_columns):
     for j in range(0,num_digit_rows):
-        font_tuple = random.choice(font_array)
+        if debug:
+          font_tuple = font_array[current_font]
+          current_font = current_font + 1
+          if current_font >= num_fonts:
+              overview_image.show()
+              sys.exit()
+
+        else:
+            font_tuple = random.choice(font_array)
+
         digit_image = create_digit(font_tuple, random.randint(0,9))
         overview_image.paste(digit_image, (digit_size*i, digit_size*j))
 
