6efe5b4e4d6c777e0ffb591120140fbbee7dd3ce
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2016-09-30 18:49:20 +0200

    Working implementation with one inception module.

--- CharacterGenerator.py
+++ CharacterGenerator.py
@@ -61,9 +61,6 @@ debug = False
 num_fonts = len(font_array)
 current_font = 0
 
-overview_image = Image.new("L", (digit_size * num_digit_columns, digit_size * num_digit_rows), 255)
-overview_draw = ImageDraw.Draw(overview_image)
-
 def add_outline(draw, x, y, font, digit, text_color):
     while True:
         outline_color = random.randint(0,255)
@@ -79,60 +76,64 @@ def add_outline(draw, x, y, font, digit, text_color):
 def displacement():
     return np.array([random.random(), random.random()]) * digit_size / 4
 
+
+def random_colors():
+    background_color = random.randint(0,255)
+    while True:
+        text_color = random.randint(0,255)
+        # find a text color that has a minimum amount of contrast against background_color:
+        if abs(text_color - background_color) > 32:
+            return background_color, text_color
+    
+def create_digit_background(background_color):
+    noise = (np.random.rand(2 * digit_size, 2 * digit_size) - 0.5) * random.randint(0,32) + background_color
+    return Image.fromarray(noise).convert("L")
+
+        
 def create_digit(font_tuple, digit):
     font = font_tuple[1]
     font_name = font_tuple[0]
 
-    if debug:
-        background_color = 255
-        text_color = 0
-    else:
-        background_color = random.randint(0,255)
-        while True:
-            text_color = random.randint(0,255)
-            # find a text color that has a minimum amount of contrast against background_color:
-            if abs(text_color - background_color) > 32:
-                break
-
-    noise = (np.random.rand(2 * digit_size, 2 * digit_size) - 0.5) * random.randint(0,32) + background_color
-    digit_image = Image.fromarray(noise).convert("L")
+    background_color, text_color = random_colors()
+    digit_image = create_digit_background(background_color)
     draw = ImageDraw.Draw(digit_image)
 
     try:
         (w,h) = font.getsize(str(digit))
     except IOError:
         print("font.getsize failed for font:%s" % font_name)
+        raise IOError
 
     x = 0.5 * (2 * digit_size - w)
     y = 0.5 * (2 * digit_size - h)
 
-    if random.random() > 0.5:
+    if random.random() > 1.0:
         add_outline(draw, x, y, font, digit, text_color)
 
     draw.text((x,y), str(digit), font=font, fill=text_color)
 
-    if not debug:
-        bounding_box = np.array([0,0,0,1,1,1,1,0]) * digit_size * 2 + (2 * np.random.rand(8) - 1) * digit_size / 4
-        transformation = ImageTransform.QuadTransform(bounding_box)
-        digit_image = digit_image.transform((digit_size * 2, digit_size * 2), transformation, resample=Image.BICUBIC)
+    bounding_box = np.array([0,0,0,1,1,1,1,0]) * digit_size * 2 + (2 * np.random.rand(8) - 1) * digit_size / 4
+    transformation = ImageTransform.QuadTransform(bounding_box)
+    digit_image = digit_image.transform((digit_size * 2, digit_size * 2), transformation, resample=Image.BICUBIC)
 
-        angle = random.randrange(-15,15)
-        digit_image = digit_image.rotate(angle, resample=Image.BICUBIC, expand = 0)
-        digit_image = digit_image.filter(ImageFilter.GaussianBlur(radius=1.5 * random.random()))
+    angle = random.randrange(-15,15)
+    digit_image = digit_image.rotate(angle, resample=Image.BICUBIC, expand = 0)
+
+    digit_image = digit_image.filter(ImageFilter.GaussianBlur(radius=1.5 * random.random()))
 
     digit_image = digit_image.crop((digit_size/2, digit_size/2, digit_size * 3 / 2, digit_size * 3 / 2))
 
-    if debug:
+    if False:
         draw = ImageDraw.Draw(digit_image)
         draw.text((0,0), str(digit), font=ImageFont.load_default(), fill=text_color)
 
     return digit_image
 
+
 def create_random_digit():
     font_tuple = random.choice(font_array)
     return create_digit(font_tuple, random.randint(0,9))
 
-default_font = ImageFont.load_default()
 
 def CharacterGenerator(batchsize):
     while True:
@@ -141,27 +142,27 @@ def CharacterGenerator(batchsize):
         for i in range(0,batchsize):
             font_tuple = random.choice(font_array)
             digit = random.randint(0,9)
-            x.append(np.array(create_digit(font_tuple, digit)).reshape(24,24,1))
-            y.append(np_utils.to_categorical([digit], nb_classes=10).reshape(10))
-        yield np.array(x),np.array(y)
+            digit_image = create_digit(font_tuple, digit)
+            x.append(np.array(digit_image).reshape(24,24,1).astype('float32') / 255.0)
+            y.append(digit)
 
-for i in range(0,num_digit_columns):
-    for j in range(0,num_digit_rows):
-        if debug:
-          font_tuple = font_array[current_font]
-          digit_image = create_digit(font_tuple, random.randint(0,9))
-          current_font = current_font + 1
-          if current_font >= num_fonts:
-              overview_image.show()
-              sys.exit()
+        yield np.array(x),np_utils.to_categorical(y,nb_classes=10)
 
-        else:
-            digit_image = create_random_digit()
-        overview_image.paste(digit_image, (digit_size*i, digit_size*j))
 
-        if debug:
-            print("%02d/%02d: %s" % (i,j, font_tuple[0]))
-            overview_draw.text((i * digit_size, j * digit_size + 22), "%02d/%02d" % (i,j))
+if __name__ == "__main__":
+    overview_image = Image.new("L", (digit_size * num_digit_columns, digit_size * num_digit_rows), 255)
+    overview_draw = ImageDraw.Draw(overview_image)
 
-overview_image.save("overview.png")
-##os.system("eog overview.png")
+    generator = CharacterGenerator(32)
+    for i in range(0,num_digit_columns):
+        j = 0
+        batch = generator.next()
+        for j in range(0,32):
+            overview_image.paste(Image.fromarray((batch[0][j].reshape(24,24) * 255).astype('uint8'),mode="L"), (digit_size*i, digit_size*j))
+#
+            if debug:
+                print("%02d/%02d: %s" % (i,j, font_tuple[0]))
+                overview_draw.text((i * digit_size, j * digit_size + 22), "%02d/%02d" % (i,j))
+
+            
+    overview_image.save("overview.png")
--- train.py
+++ train.py
@@ -1,19 +1,44 @@
 import CharacterGenerator
-import keras
-from keras.layers import Input, Dense, Convolution2D, Flatten, Reshape
-from keras.models import Model
+from keras.layers import Input, merge
+from keras.layers import Dense, Dropout, Convolution2D, MaxPooling2D, Flatten, Reshape, Activation
+from keras.models import Model, Sequential
+from keras.optimizers import SGD
 
-# this returns a tensor
-inputs = Input(shape=(24,24,1))
-x = Convolution2D(64, 3, 3, border_mode="same", activation="relu")(inputs)
-x = Convolution2D(64, 3, 3, border_mode="same", activation="relu")(x)
-x = Flatten()(x)
-predictions = Dense(10, activation='softmax')(x)
+input = Input(shape=(24,24,1))
+tower_1 = Convolution2D(64, 1, 1, border_mode='same', activation='relu')(input)
+tower_1 = Convolution2D(64, 3, 3, border_mode='same', activation='relu')(tower_1)
+tower_2 = Convolution2D(64, 1, 1, border_mode='same', activation='relu')(input)
+tower_2 = Convolution2D(64, 5, 5, border_mode='same', activation='relu')(tower_2)
+tower_3 = MaxPooling2D((3, 3), strides=(1, 1), border_mode='same')(input)
+tower_3 = Convolution2D(64, 1, 1, border_mode='same', activation='relu')(tower_3)
+tower_4 = Convolution2D(64, 1, 1, border_mode='same', activation='relu')(input)
+output = merge([tower_1, tower_2, tower_3, tower_4], mode='concat', concat_axis=1)
+inception1 = Model(input, output)
 
-model = Model(input=inputs, output=predictions)
-model.compile(optimizer='adagrad',
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
+model = Sequential()
+model.add(inception1)
+model.add(Dropout(0.5))
 
-generator = CharacterGenerator.CharacterGenerator(32)
-model.fit_generator(generator, 1000, 40)  # starts training
+model.add(Flatten())
+model.add(Dense(256))
+model.add(Activation("relu"))
+model.add(Dropout(0.5))
+model.add(Dense(10, init='uniform'))
+model.add(Activation('softmax'))
+
+model.compile(
+    optimizer='adagrad',
+    loss='categorical_crossentropy',
+    metrics=['accuracy'])
+
+batch_size = 32
+generator = CharacterGenerator.CharacterGenerator(batch_size)
+
+model.fit_generator(
+    generator, batch_size * 512, 1000,
+    validation_data = generator,
+    nb_val_samples = batch_size * 32,
+    max_q_size=16, nb_worker=8, pickle_safe=True)  # starts training
+
+
+
