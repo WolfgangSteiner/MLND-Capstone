989a9d78024eff1091ad38f436f0f966ecfc0f22
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2016-10-12 20:26:35 +0200

    test_svhn.py takes model as argument

--- test_svhn.py
+++ test_svhn.py
@@ -1,9 +1,10 @@
 import numpy as np
 from keras.models import load_model
 from Common import load_svhn
+from sys import argv
 
 print "loading model..."
-model=load_model('checkpoint.hdf5')
+model=load_model(argv[1])
 print model.metrics_names
 print "loading test data..."
 X,y = load_svhn('test_32x32.mat')
