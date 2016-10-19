b835ae512aa9e86ea687b194b6ad1862478b1746
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2016-10-19 18:10:44 +0200

    add test_svhn_extra.py

---
+++ test_svhn_extra.py
@@ -0,0 +1,12 @@
+import numpy as np
+from keras.models import load_model
+from Common import load_svhn
+from sys import argv
+
+print "loading model..."
+model=load_model(argv[1])
+print model.metrics_names
+print "loading test data..."
+X,y = load_svhn('extra_32x32.mat')
+print "evaluating model..."
+print model.evaluate(X,y)
