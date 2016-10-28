ada16a54fd392e57473905e27270efec53d68372
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2016-10-28 15:34:21 +0200

    compare multiple learning curves

--- compare_learning_curves.py
+++ compare_learning_curves.py
@@ -2,9 +2,11 @@ import numpy as np
 import pandas as pd
 from sys import argv
 import matplotlib.pyplot as plt
+from time import sleep
 
 data=[]
 names=[]
+
 for f in argv[1:]:
     names.append(f)
     data.append(pd.read_csv(f+".log"))
@@ -23,19 +25,31 @@ numplots = 3 if show_lr() else 2
 
 figure, ax = plt.subplots(numplots)
 #plt.subplot(111)
+plt.ion()
 
 
-for i in range(0,len(names)):
-    ax[0].plot(data[i]['epoch'], data[i]['acc'], label= names[i] + " (tr)")
-    ax[0].plot(data[i]['epoch'], data[i]['val_acc'], label=names[i] + " (val)")
-    ax[1].plot(data[i]['epoch'], data[i]['loss'], label= names[i] + " (tr)")
-    ax[1].plot(data[i]['epoch'], data[i]['val_loss'], label=names[i] + " (val)")
+while True:
+    ax[0].clear()
+    ax[1].clear()
 
-    if show_lr():
-        ax[2].plot(data[i]['epoch'], data[i]['lr'])
-        ax[2].set_yscale('log')
+    data=[]
+    for f in names:
+        data.append(pd.read_csv(f+".log"))
 
-ax[0].legend(loc="lower right")
-ax[1].legend()
-plt.xlabel("number of epochs")
-plt.show()
+    for i in range(0,len(names)):
+        ax[0].plot(data[i]['epoch'], data[i]['acc'], label= names[i] + " (tr)")
+        ax[0].plot(data[i]['epoch'], data[i]['val_acc'], label=names[i] + " (val)")
+        ax[1].plot(data[i]['epoch'], data[i]['loss'], label= names[i] + " (tr)")
+        ax[1].plot(data[i]['epoch'], data[i]['val_loss'], label=names[i] + " (val)")
+
+        if show_lr():
+            ax[2].plot(data[i]['epoch'], data[i]['lr'])
+            ax[2].set_yscale('log')
+            
+
+    ax[0].legend(loc="lower right")
+    ax[1].legend()
+    plt.xlabel("number of epochs")
+    plt.draw()
+    plt.pause(1)
+
