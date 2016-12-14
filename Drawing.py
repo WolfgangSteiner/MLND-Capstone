8c5285a71ab0fdb3b16a3188d6edb5d676a69a5a
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2016-12-14 13:00:05 +0100

    more tweaks to random background

--- Drawing.py
+++ Drawing.py
@@ -11,10 +11,20 @@ def random_blur(img, min_blur, max_blur):
     return blur(img, random.uniform(min_blur, max_blur))
 
 
-def create_noise_background(size, min_color, max_color, blur_radius, min_factor=1.0, max_factor=32.0):
+def create_noise_background(size, text_color, background_color, min_color_delta, blur_radius, min_factor=1.0, max_factor=32.0):
+    if background_color < text_color:
+        min_color = 0
+        max_color = text_color - min_color_delta
+    else:
+        min_color = text_color + min_color_delta
+        max_color = 255
+
+    background_color = random.uniform(min_color, max_color)
+    noise_amp = min(abs(max_color - background_color), abs(background_color - min_color))
+
     wn = int(size.x / random.uniform(min_factor, max_factor))
     hn = int(size.y / random.uniform(min_factor, max_factor))
-    noise = (np.random.rand(hn,wn) - 0.5) * (max_color - min_color) + min_color
+    noise = np.random.rand(hn,wn) * noise_amp + background_color
     noise = np.clip(noise, 0.0, 255.0)
     img = Image.fromarray(noise).convert('RGBA')
     img = blur(img, blur_radius)
