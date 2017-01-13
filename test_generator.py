42cb491cacff05eec7cb37ad7a53cb6db271edf0
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2017-01-13 17:04:14 +0100

    reduce number of test examples, show progress

--- test_generator.py
+++ test_generator.py
@@ -8,6 +8,6 @@ char_size = 32
 print "Loading model..."
 model=load_model(argv[1])
 print "Generating test data..."
-X,y = CharacterGenerator.CharacterGenerator(26000).next()
+X,y = CharacterGenerator.CharacterGenerator(8192, {'show_progress':True}).next()
 print "Evaluating model..."
 print model.evaluate(X,y)
