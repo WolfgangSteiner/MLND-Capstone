2a5fa29fe36b668611c596573abf1e9829d0155a
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2017-01-13 18:57:25 +0100

    add new test script

---
+++ prepare_svhn_digit_data.py
@@ -0,0 +1,147 @@
+import h5py
+import numpy as np
+import pickle
+import sys
+import argparse
+import Utils
+from PIL import Image
+from keras.utils.np_utils import to_categorical
+
+
+def open_image(dir, img_name):
+    return Image.open(dir + '/' + img_name + ".png")
+
+
+def unpack_int(hdf5_file, bbox, field):
+    dataset_or_ref = bbox.get(field)
+
+    if type(dataset_or_ref) is h5py.Dataset:
+        dataset = dataset_or_ref
+        data = dataset[0][0]
+        if type(data) is h5py.Reference:
+            return int(np.array(hdf5_file.get(data))[0][0])
+        else:
+            return int(dataset[0])
+    else:
+        raise IOError
+
+
+def deref_array(hdf5_file, array):
+    result = []
+    for ref in array:
+        ref = ref[0]
+        digit = int(np.array(hdf5_file.get(ref))[0][0])
+        if digit == 10:
+            digit = 0
+        result.append(digit)
+    return np.array(result)
+
+
+def label_from_dataset(hdf5_file, dataset):
+    array = np.array(dataset)
+    if array.dtype == 'float64':
+        return np.array([int(array[0][0])])
+    else:
+        return deref_array(hdf5_file, array)
+
+
+def get_bbox_data_single(dataset, key):
+    return dataset.get(key)[0][0]
+
+
+def get_bbox_data(hdf5_file, dataset, key, index):
+    ref = dataset.get(key)[index][0]
+    return np.array(hdf5_file.get(ref))[0][0]
+
+
+def unpack_bboxes(hdf5_file, dataset):
+    array = np.array(dataset)
+    hdata = dataset.get('height')
+    num_bboxes = hdata.shape[0]
+    bboxes = []
+
+    if num_bboxes == 1:
+        h = get_bbox_data_single(dataset, 'height')
+        w = get_bbox_data_single(dataset, 'width')
+        x = get_bbox_data_single(dataset, 'left')
+        y = get_bbox_data_single(dataset, 'top')
+        l = get_bbox_data_single(dataset, 'label')
+        l -= (10 if l == 10 else 0)
+        bboxes.append([int(x), int(y), int(w), int(h), int(l)])
+    else:
+        for i in range(0,num_bboxes):
+            h = get_bbox_data(hdf5_file, dataset, 'height', i)
+            w = get_bbox_data(hdf5_file, dataset, 'width', i)
+            x = get_bbox_data(hdf5_file, dataset, 'left', i)
+            y = get_bbox_data(hdf5_file, dataset, 'top', i)
+            l = get_bbox_data(hdf5_file, dataset, 'label', i)
+            l -= (10 if l == 10 else 0)
+            bboxes.append([int(x), int(y), int(w), int(h), int(l)])
+
+    return bboxes
+
+def unpack_label(hdf5_file, bbox):
+    dataset_or_ref = bbox.get('label')
+    dataset = None
+    if type(dataset_or_ref) is h5py.Dataset:
+        dataset = dataset_or_ref
+    else:
+        ref = dataset_or_ref
+        dataset = hdf5_file[ref]
+
+    return label_from_dataset(hdf5_file, dataset)
+
+
+def join_labels(bbox_array):
+    label = ""
+    for bbox in bbox_array:
+        label += str(int(bbox[4]))
+
+    return label
+
+
+def prepare_svhn_digit_data(dir):
+    f = h5py.File(dir + '/digitStruct.mat', 'r')
+    refs = f.get('#refs#')
+    name_refs = f['/digitStruct/name']
+    bbox_refs = f['/digitStruct/bbox']
+    num_data = len(name_refs)
+    labels = {}
+    bboxes = {}
+
+    print "Reading digitStruct.mat..."
+    X = []
+    y = []
+
+    for i in range(0,num_data):
+        Utils.progress_bar(i+1,num_data)
+        name_ref = name_refs[i][0]
+        bbox_ref = bbox_refs[i][0]
+        file_name_array = np.array(f.get(name_ref))
+        file_name = "".join([chr(c) for c in file_name_array])
+        bbox_data = f.get(bbox_ref)
+        bbox_array = unpack_bboxes(bbox_data, bbox_data)
+        id = file_name.replace('.png', '')
+        img = open_image(dir, id)
+
+        for bx,by,w,h,label in bbox_array:
+            char_img = img.crop((bx,by,bx+w,by+h))
+            char_img = char_img.resize((32,32), resample=Image.BILINEAR)
+            char_img = char_img.convert('L')
+            X.append(np.array(char_img).astype(np.float32).reshape((32,32,1)) / 255.0)
+            y.append(label)
+
+    X = np.array(X)
+    y = to_categorical(np.array(y), 10)
+
+    with open("char_classifier_%s_svhn.pickle" % (dir), "wb") as f:
+        print("Writing data to char_classifier_%s_svhn.pickle..." % (dir))
+        pickle.dump(X, f)
+        pickle.dump(y, f)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dir', action="store")
+    args = parser.parse_args()
+    prepare_svhn_digit_data(args.dir)
---
+++ test_char_classifier_on_svhn_test_data.py
@@ -0,0 +1,25 @@
+import Utils
+import os
+import sys
+from prepare_svhn_digit_data import prepare_svhn_digit_data
+from test_model import test_model
+from plot_data import plot_data
+
+data_file = "char_classifier_test_svhn.pickle"
+if not os.path.exists(data_file):
+    print("")
+    print("")
+    print("This script will test the character classifier on test data from the street view house numbers.")
+    print("Downloading and convertig the data might take a while.")
+    print("If you already have the file 'http://ufldl.stanford.edu/housenumbers/test.tar.gz', place it in this directory.")
+
+    if os.path.exists("test") or Utils.query_yes_no("Continue?"):
+        Utils.download_and_extract("test", "test.tar.gz", "http://ufldl.stanford.edu/housenumbers/test.tar.gz")
+        prepare_svhn_digit_data("test")
+        print("Plotting some examples to char_classifier_test_svhn.png ...")
+        plot_data(data_file)
+    else:
+        sys.exit(0)
+
+
+test_model("char_classifier_2.hdf5", data_file, 8192)
