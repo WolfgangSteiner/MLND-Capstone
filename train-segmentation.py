1fc4eea27bca606e75f5eb8994da6ab29fe9c571
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2017-01-15 22:33:40 +0100

    update segmentation model

--- train-segmentation.py
+++ train-segmentation.py
@@ -17,4 +17,5 @@ t.dense(256)
 t.dense(256)
 t.binary_classifier()
 options={'max_blur':2.5, 'max_rotation':15, 'min_size': 0.5, 'min_color_delta': 8, 'min_noise':4, 'max_noise':8}
+options['num_epochs'] = 200
 t.train_segmentation_generator(options=options)
